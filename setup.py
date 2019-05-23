a5879f13d4c3c0ce620e437f03662c87811dce4a
hawry <hawry@hawry.net>
2019-05-23 18:56:57 +0200

    initial setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import re
+import os
+
+with open("README.md","r") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name="jirator",
+    version="0.1.0",
+    author="hawry",
+    entry_points = {
+        "console_scripts": ["jirator=jirator.jirator.main"]
+    },
+    author_email="hawry@hawry.net",
+    description="Fetch your assigned JIRA issues and checkout new branches with their ID",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hawry/jirator",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "console-menu",
+        "jira"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 3 - Alpha"
+    ]
+)
